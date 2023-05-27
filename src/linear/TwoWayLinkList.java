8cf5174c30d7d8689b0306d3f9ea9c631a44af01
qzh <2806894439@qq.com>
2023-05-27 23:02:48 +0800

    20230527链表复杂度分析

--- src/linear/TwoWayLinkList.java
+++ src/linear/TwoWayLinkList.java
@@ -1,6 +1,8 @@
 package linear;
 
-public class TwoWayLinkList<T> {
+import java.util.Iterator;
+
+public class TwoWayLinkList<T> implements Iterable<T>{
     //首结点
     private Node head;
     //最后一个结点
@@ -69,11 +71,113 @@ public class TwoWayLinkList<T> {
 
     //插入元素t
     public void insert(T t){
-        
+        //如果链表为空
+        if (isEmpty()){
+            //创建新的结点
+            Node newNode = new Node(t, head, null);
+
+            //让新结点成为尾结点
+            last=newNode;
+
+            //让头结点指向尾结点
+            head.next=last;
+        }else {
+            //如果链表不为空
+            //创建新的结点
+            Node<T> newNode = new Node<>(t, last, null);
+
+            //让当前的尾结点指向新结点
+            last.next=newNode;
+
+            //让新结点成为尾结点
+            last=newNode;
+        }
+
+        //元素个数+1
+        N++;
     }
 
     //向指定位置i处插入元素t
     public void insert(int i,T t){
+        //找到i位置的前一个结点
+        Node preNode=head;
+        for (int index = 0; index < i; index++) {
+            preNode=preNode.next;
+        }
+        //找到i位置的结点
+        Node cur=preNode.next;
+        //创建新结点
+        Node newNode = new Node(t,preNode,cur);
+        //让i位置的前一个结点的next结点指向新结点
+        preNode.next=newNode;
+        //让i位置的后一个结点的pre结点指向新结点
+        cur.pre=newNode;
+        //元素个数+1
+        N++;
+    }
 
+    //获取指定位置i处的元素
+    public T get(int i){
+        Node n=head.next;
+        for (int index = 0; index < i; index++) {
+            n=n.next;
+        }
+        return (T) n.item;
+    }
+
+    //找到元素t在链表中第一次出现的位置
+    public int indexOf(T t){
+        Node n=head;
+        for (int i = 0; n.next!=null; i++) {
+            n=n.next;
+            if (n.item.equals(t)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //删除位置i处的元素，并返回该元素
+    public T remove(int i){
+        //找到i位置的前一个结点
+        Node preNode=head;
+        for (int index = 0; index < i; index++) {
+            preNode=preNode.next;
+        }
+        //找到i位置的结点
+        Node cur=preNode.next;
+        //找到i位置的下一个结点
+        Node nextNode = cur.next;
+        //让i位置的前一个结点的next指向i位置的下一个结点
+        preNode.next=nextNode;
+        //让i位置的下一个结点的pre指向i位置的前一个结点
+        nextNode.pre=preNode;
+        //元素个数-1
+        N--;
+        return (T) cur.item;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new TIterator();
+    }
+
+    private class TIterator implements Iterator{
+        private Node n;
+
+        public TIterator() {
+            this.n=head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return n.next!=null;
+        }
+
+        @Override
+        public Object next() {
+            n=n.next;
+            return n.item;
+        }
     }
 }
