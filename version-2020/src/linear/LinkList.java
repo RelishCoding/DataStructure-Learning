d3877027f21404efb514160ebb6e159b4397542f
qzh <2806894439@qq.com>
2023-12-20 19:51:19 +0800

    单向链表

---
+++ version-2020/src/linear/LinkList.java
@@ -0,0 +1,175 @@
+package linear;
+
+import java.util.Iterator;
+
+public class LinkList<T> implements Iterable<T>{
+    //记录头结点
+    private Node head;
+    //记录链表的长度
+    private int N;
+
+    //节点类
+    private class Node{
+        //存储数据
+        T item;
+        //下一个结点
+        Node next;
+
+        public Node(T item,Node next){
+            this.item=item;
+            this.next=next;
+        }
+    }
+
+    public LinkList() {
+        //初始化头结点
+        this.head=new Node(null,null);
+        //初始化元素个数
+        this.N=0;
+    }
+
+    //清空链表
+    public void clear(){
+        head.next=null;
+        N=0;
+    }
+
+    //获取链表的长度
+    public int length(){
+        return N;
+    }
+
+    //判断链表是否为空
+    public boolean isEmpty(){
+        return N==0;
+    }
+
+    //获取指定位置i处的元素
+    public T get(int i){
+        //通过循环，从头节点开始往后找，依次找i次，就可以找到对应的元素
+        Node n=head.next;
+        for (int index = 0; index < i; index++) {
+            n=n.next;
+        }
+        return n.item;
+    }
+
+    //向链表中添加元素t
+    public void insert(T t){
+        //找到当前最后一个结点
+        Node n=head;
+        while(n.next!=null){
+            n=n.next;
+        }
+
+        //创建新结点
+        Node newNode = new Node(t,null);
+        //让当前最后一个结点指向新结点
+        n.next=newNode;
+        //元素的个数+1
+        N++;
+    }
+
+    //向指定位置i处添加元素t
+    public void insert(int i,T t){
+        //找到i位置的前一个节点
+        Node pre = head;
+        for (int index = 0; index <= i - 1; index++) {
+            pre=pre.next;
+        }
+
+        //找到i位置的结点
+        Node cur=pre.next;
+
+        //创建新结点，并且新结点需要指向原来i位置的结点
+        Node newNode=new Node(t,cur);
+
+        //原来i位置的前一个结点指向新结点即可
+        pre.next=newNode;
+
+        //元素个数+1
+        N++;
+    }
+
+    //删除指定位置i处的元素，并返回被删除的元素
+    public T remove(int i){
+        //找到i位置的前一个节点
+        Node pre = head;
+        for (int index = 0; index <= i - 1; index++) {
+            pre=pre.next;
+        }
+
+        //找到i位置的结点
+        Node cur=pre.next;
+
+        //找到i位置的下一个结点
+        Node nextNode = cur.next;
+
+        //前一个结点指向下一个结点
+        pre.next=nextNode;
+
+        //元素个数-1
+        N--;
+        return cur.item;
+    }
+
+    //查找元素t在链表中第一次出现的位置
+    public int indexOf(T t){
+        //从头结点开始，依次找到每一个结点，取出item和t比较，如同相同则找到
+        Node n=head;
+        for (int i = 0; n.next!=null; i++) {
+            n=n.next;
+            if (n.item.equals(t)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new LIterator();
+    }
+
+    public class LIterator implements Iterator{
+        private Node n;
+        public LIterator(){
+            this.n=head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return n.next!=null;
+        }
+
+        @Override
+        public Object next() {
+            n=n.next;
+            return n.item;
+        }
+    }
+
+    //用来反转整个链表
+    public void reverse(){
+        //判断当前链表是否为空，如果为空则结束运行，如果不是则调用重载的reverse方法进行反转
+        if (isEmpty()){
+            return;
+        }
+        reverse(head.next);
+    }
+
+    //反转指定的结点cur，并把反转后的结点返回
+    public Node reverse(Node cur){
+        if (cur.next==null){
+            head.next=cur;
+            return cur;
+        }
+        //递归反转当前结点的下一个结点，返回值就是链表反转后当前节点的上一个结点
+        Node preNode=reverse(cur.next);
+        //让返回的结点的next指向当前结点cur
+        preNode.next=cur;
+        //让当前结点的next指向null
+        cur.next=null;
+        return cur;
+    }
+}
