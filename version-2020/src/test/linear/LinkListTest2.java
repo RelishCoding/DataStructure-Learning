996a01e4433c86757048b17c6fb8fd90017c1071
qzh <2806894439@qq.com>
2023-12-20 19:58:02 +0800

    测试反转单链表

---
+++ version-2020/src/test/linear/LinkListTest2.java
@@ -0,0 +1,27 @@
+package test.linear;
+
+import linear.LinkList;
+
+public class LinkListTest2 {
+    public static void main(String[] args) {
+        //创建单向链表对象
+        LinkList<String> sl = new LinkList<>();
+
+        //测试插入
+        sl.insert("姚明");
+        sl.insert("科比");
+        sl.insert("麦迪");
+        sl.insert(1,"艾弗森");
+        System.out.println(sl);
+        for (String s : sl) {
+            System.out.println(s);
+        }
+
+        System.out.println("------------");
+
+        sl.reverse();
+        for (String s : sl) {
+            System.out.println(s);
+        }
+    }
+}
