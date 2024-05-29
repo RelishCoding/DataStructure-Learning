4e16eec3134ad1f181242fdd4260387c2b459191
qzh <2806894439@qq.com>
2024-05-29 21:38:39 +0800

    有序符号表测试

---
+++ version-2020/src/test/symbol/OrderSymbolTableTest.java
@@ -0,0 +1,16 @@
+package test.symbol;
+
+import symbol.OrderSymbolTable;
+
+public class OrderSymbolTableTest {
+    public static void main(String[] args) {
+        OrderSymbolTable<Integer, String> orderSymbolTable = new OrderSymbolTable<>();
+
+        orderSymbolTable.put(1, "张三");
+        orderSymbolTable.put(2, "李四");
+        orderSymbolTable.put(4, "赵六");
+        orderSymbolTable.put(7, "田七");
+
+        orderSymbolTable.put(3, "王五");
+    }
+}
